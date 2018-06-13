f39b44eacff59edbade023db2d37e60a685b7610
Paul <pcherian10@gmail.com>
2018-06-13 20:48:06 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 8) do
+ActiveRecord::Schema.define(version: 7) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 8) do
     t.integer "network_id"
     t.string "day"
     t.string "season"
-    t.string "genre"
   end
 
 end
