28c20fba7d89fb5a7bc045d7bc1d745c4174f3ec
Paul <pcherian10@gmail.com>
2018-06-13 18:27:30 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,7 +1,7 @@
 class CreateCharacters < ActiveRecord::Migration[4.2]
 
     def change
-      create_characters :characters do |t|
+      create_table :characters do |t|
         t.string :name
         t.string :catchphrase
         t.integer :show_id
